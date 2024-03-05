3ad9c44cc1754744ddb746a30fb595e043a00a96
yokiowiiiz <159612037+yokiowiiiz@users.noreply.github.com>
2024-03-05 14:15:18 +0300

    Update boolean.js

--- boolean.js
+++ boolean.js
@@ -3,36 +3,30 @@ function inPositive(x1) {
 }
 console.log(inPositive(-1))
 
-
 function isOdd(x1) {
     return Boolean(x1 % 2 != 1 && x1 % 2 != -1)
 }
 console.log(isOdd(3))
-
 
 function checkInequality(x1, x2) {
     return Boolean(x1 > 2 && x2 <=3)
 }
 console.log(checkInequality(4, 3))
 
-
 function checkInequality2(x1, x2) {
     return Boolean(x1 >= 0 && x2 <= -2)
 }
 console.log(checkInequality2(-1, -2))
-
 
 function checkBetweenNumbers(x1, x2, x3) {
     return Boolean(x1 < x2 && x2 < x3)
 }
 console.log(checkBetweenNumbers(5, 5, 8))
 
-
 function checkOddThreeDigitNumber(x1) {
     return Boolean(x1 / 100 >= 1 && x1 % 2 == 1 && x1 % 2 == -1)
 }
 console.log(checkOddThreeDigitNumber(-789))
-
 
 function checkUniqeDigits(x1) {
     let x2 = Math.abs(x1);
@@ -40,22 +34,18 @@ function checkUniqeDigits(x1) {
 {
 console.log(checkUniqeDigits(-77))
 
-
 function isSecondQuadrant(x1, y) {
     return Boolean(x1 < 0 && y > 0)
 }
 console.log(isSecondQuadrant(-2, 3))
-
 
 function isIsoscelesTriangle(x1, x2, x3) {
     return Boolean(x1 == x2 || x2 == x3 || x1 == x3)
 }
 console.log(isIsoscelesTriangle(5, 5, 6))
 
-
 function areSameColorFields(x1, y1, x2, y2) {
     return Boolean((x1+y1)%2 == 0 && (x2+y2)%2 == 0)
 }
 console.log(areSameColorFields(1, 1, 2, 2))
-
 export{inPositive, isOdd, checkInequality, checkInequality2, checkBetweenNumbers, checkOddThreeDigitNumber, checkUniqeDigits, isSecondQuadrant, isIsoscelesTriangle, areSameColorFields}
