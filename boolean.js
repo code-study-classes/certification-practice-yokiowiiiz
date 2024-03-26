fbc8e7de4e389ac06c5da2fd7636c4abbaf6e486
yokiowiiiz <159612037+yokiowiiiz@users.noreply.github.com>
2024-03-26 11:37:05 +0300

    Update boolean.js

--- boolean.js
+++ boolean.js
@@ -1,51 +1,61 @@
-function inPositive(x1) {
-    return Boolean(x1 > 0)
+function isPositive(x1) {
+    return Boolean(x1 > 0);
 }
-console.log(inPositive(-1))
+console.log(isPositive(-1));
 
 function isOdd(x1) {
-    return Boolean(x1 % 2 != 1 && x1 % 2 != -1)
+    x1 = Math.abs(x1);
+    return Boolean(x1 % 2 === 1);
 }
-console.log(isOdd(3))
+console.log(isOdd(5));
 
 function checkInequality(x1, x2) {
-    return Boolean(x1 > 2 && x2 <=3)
+    return Boolean(x1 > 2 && x2 <=3);
 }
-console.log(checkInequality(4, 3))
+console.log(checkInequality(4, 3));
 
 function checkInequality2(x1, x2) {
-    return Boolean(x1 >= 0 && x2 <= -2)
+    return Boolean(x1 >= 0 || x2 < -2);
 }
-console.log(checkInequality2(-1, -2))
+
+console.log(checkInequality2(4, -3));
 
 function checkBetweenNumbers(x1, x2, x3) {
     return Boolean(x1 < x2 && x2 < x3)
 }
-console.log(checkBetweenNumbers(5, 5, 8))
+console.log(checkBetweenNumbers(5, 5, 8));
 
 function checkOddThreeDigitNumber(x1) {
-    return Boolean(x1 / 100 >= 1 && x1 % 2 == 1 && x1 % 2 == -1)
+    x1 = Math.abs(x1);
+    return Boolean(x1 > 100 && x1 % 2 === 1);
 }
-console.log(checkOddThreeDigitNumber(-789))
+console.log(checkOddThreeDigitNumber(135));
 
-function checkUniqeDigits(x1) {
-    let x2 = Math.abs(x1);
-    return Boolean((Math.floor(x2 / 100) !== x2 % 10 && x2 % 10 !== Math.floor(x2 % 100 / 10) && Math.floor(x2 / 100) !== Math.floor(x2 % 100 / 10)));
-{
-console.log(checkUniqeDigits(-77))
+function checkUniqueDigits(number) {
+    number = Math.abs(number);
+    if (number < 100 || number > 999) {
+    return false;
+  }
+    return Boolean((Math.floor(number/100) != number%10 && number%10 != Math.floor(number%100/10) && Math.floor(number/100) != Math.floor(number%100/10)));
+}
+console.log(checkUniqueDigits(-1234));
 
 function isSecondQuadrant(x1, y) {
-    return Boolean(x1 < 0 && y > 0)
+    return Boolean(x1 < 0 && y > 0);
 }
-console.log(isSecondQuadrant(-2, 3))
+console.log(isSecondQuadrant(-2, 3));
 
 function isIsoscelesTriangle(x1, x2, x3) {
-    return Boolean(x1 == x2 || x2 == x3 || x1 == x3)
+    if (x1 == x2 && x2 == x3 && x1 == x3) {
+        return false;
+    }
+    return Boolean(x1 == x2 || x2 == x3 || x1 == x3);
 }
-console.log(isIsoscelesTriangle(5, 5, 6))
+console.log(isIsoscelesTriangle(5, 5, 5));
 
 function areSameColorFields(x1, y1, x2, y2) {
-    return Boolean((x1+y1)%2 == 0 && (x2+y2)%2 == 0)
+return (x1 + y1) % 2 === (x2 + y2) % 2;
 }
-console.log(areSameColorFields(1, 1, 2, 2))
-export{inPositive, isOdd, checkInequality, checkInequality2, checkBetweenNumbers, checkOddThreeDigitNumber, checkUniqeDigits, isSecondQuadrant, isIsoscelesTriangle, areSameColorFields}
+
+console.log(areSameColorFields(1, 1, 2, 2));
+export{isPositive, isOdd, checkInequality, checkInequality2, checkBetweenNumbers, checkOddThreeDigitNumber, checkUniqueDigits, isSecondQuadrant, isIsoscelesTriangle, areSameColorFields};
